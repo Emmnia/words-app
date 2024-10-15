702fb5a10efeba5606af0ccb817cdb004e1101ed
Emmnia <eugmalikova@gmail.com>
2024-10-15 21:48:32 +0300

    fix default props

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -6,51 +6,6 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
 
-export const Slider = ({ initialSlideIndex = 0, wordsData = wordsBackUp }) => {
-    const [slideIndex, setSlideIndex] = useState(initialSlideIndex);
-    const [animation, setAnimation] = useState(" ");
-
-    wordsData = words
-
-    const handlePrevClick = () => {
-        setAnimation("previous");
-        setTimeout(() => {
-            setSlideIndex((slideIndex - 1 + wordsData.length) % wordsData.length);
-            setAnimation(" ");
-        }, 500);
-    };
-
-    const handleNextClick = () => {
-        setAnimation("next");
-        setTimeout(() => {
-            setSlideIndex((slideIndex + 1) % wordsData.length);
-            setAnimation(" ");
-        }, 500);
-    };
-
-    return (
-        <div className="slider-container">
-            <button className="slider__button" onClick={handlePrevClick}> <FontAwesomeIcon icon={faArrowLeft} /> </button>
-            <div className={"slider__content " + animation}>
-                {wordsData.map((word, index) => (
-                    <Card
-                        key={word.id}
-                        id={word.id}
-                        english={word.english}
-                        transcription={word.transcription}
-                        russian={word.russian}
-                        tags={word.tags}
-                        boolean={word.boolean}
-                        index={index}
-                        visible={index === slideIndex}
-                    />
-                ))}
-            </div>
-            <button className="slider__button" onClick={handleNextClick}> <FontAwesomeIcon icon={faArrowRight} /> </button>
-        </div>
-    )
-}
-
 const wordsBackUp = [{
     id: "16334",
     english: "cat",
@@ -97,3 +52,50 @@ const wordsBackUp = [{
     boolean: "false"
 }
 ]
+
+export const Slider = ({ initialSlideIndex = 0, wordsData = wordsBackUp }) => {
+    const [slideIndex, setSlideIndex] = useState(initialSlideIndex);
+    const [animation, setAnimation] = useState(" ");
+
+    wordsData = words
+
+    const handlePrevClick = () => {
+        setAnimation("previous");
+        setTimeout(() => {
+            setSlideIndex((slideIndex - 1 + wordsData.length) % wordsData.length);
+            setAnimation(" ");
+        }, 500);
+    };
+
+    const handleNextClick = () => {
+        setAnimation("next");
+        setTimeout(() => {
+            setSlideIndex((slideIndex + 1) % wordsData.length);
+            setAnimation(" ");
+        }, 500);
+    };
+
+    return (
+        <div className="slider-container">
+            <button className="slider__button" onClick={handlePrevClick}> <FontAwesomeIcon icon={faArrowLeft} /> </button>
+            <div className={"slider__content " + animation}>
+                {wordsData.map((word, index) => (
+                    <Card
+                        key={word.id}
+                        id={word.id}
+                        english={word.english}
+                        transcription={word.transcription}
+                        russian={word.russian}
+                        tags={word.tags}
+                        boolean={word.boolean}
+                        index={index}
+                        visible={index === slideIndex}
+                    />
+                ))}
+            </div>
+            <button className="slider__button" onClick={handleNextClick}> <FontAwesomeIcon icon={faArrowRight} /> </button>
+        </div>
+    )
+}
+
+
