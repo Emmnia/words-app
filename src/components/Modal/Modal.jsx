8c22f7df3ec95675870cbeb561876d9cb21bb008
Eugenia <110728681+Emmnia@users.noreply.github.com>
2024-12-08 19:50:30 +0300

    tmp

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,95 +1,60 @@
-import Dialog from '@mui/material/Dialog';
-import DialogContent from '@mui/material/DialogContent';
-import DialogActions from '@mui/material/DialogActions';
-import IconButton from '@mui/material/IconButton';
-import CloseIcon from '@mui/icons-material/Close';
-import { observer } from 'mobx-react-lite';
-import { forwardRef, useState, useEffect } from 'react';
-import { wordsStore } from '../../store/words-store';
+import { ModalWrapper, ModalHeader, ModalFooter, ModalButton, ModalButtonIcon } from './Modal.styled'
 import { Checkbox } from '../Checkbox/Checkbox';
-import { Card } from '../Card/Card';
-import { Loader } from '../Loader/Loader';
+import { forwardRef, useEffect } from 'react';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import { useLocalStorage } from "@uidotdev/usehooks";
 import { v4 as uuidv4 } from 'uuid';
+import { Card } from '../Card/Card';
+import { Loader } from '../Loader/Loader'
+import { observer } from 'mobx-react-lite';
+import { wordsStore } from '../../store/words-store';
 
 export const Modal = observer(forwardRef(function Modal({ onClose }, ref) {
     const checkboxId = uuidv4();
 
     const { word, loading, wordUpdated } = wordsStore;
 
-    const [showModalToday, setShowModalToday] = useState(() => {
-        const storedValue = localStorage.getItem('showModalToday');
-        return storedValue !== 'false';
-    });
+    const [showModalToday, setShowModalToday] = useLocalStorage('showModalToday', null);
 
     const handleModalCheck = (event) => {
         const isChecked = event.target.checked;
-        const newValue = !isChecked;
-        setShowModalToday(newValue);
-        localStorage.setItem('showModalToday', String(newValue));
-    };
-
-    const handleClose = () => {
-        setShowModalToday(false);
-        localStorage.setItem('showModalToday', 'false');
-        onClose?.();
-    };
-
-    const handleOpen = () => {
-        setShowModalToday(true);
-        localStorage.setItem('showModalToday', 'true');
+        setShowModalToday(!isChecked);
     };
 
     useEffect(() => {
+        if (showModalToday && ref.current) {
+            ref.current.showModal();
+        }
+    }, [ref, showModalToday]);
+
+    useEffect(() => {
+        console.log('wordUpdated:', wordUpdated);
+        console.log('word:', word);
         if (wordUpdated) {
-            handleOpen();
+            setShowModalToday(true);
             wordsStore.setWordUpdated(false);
         }
-    }, [wordUpdated]);
+    }, [setShowModalToday, wordUpdated, word]);
 
     return (
-        <Dialog
-            open={showModalToday}
-            onClose={handleClose}
-            fullWidth
-            maxWidth="xs"
-            PaperProps={{
-                sx: {
-                    borderRadius: '10px',
-                    padding: '20px',
-                    textAlign: 'center',
-                    position: 'relative'
-                }
-            }}
-        >
-            <IconButton
-                onClick={handleClose}
-                sx={{
-                    position: 'absolute',
-                    right: 8,
-                    top: 8,
-                    color: '#E55D87',
-                    border: '1px solid #E55D87'
-                }}
-            >
-                <CloseIcon />
-            </IconButton>
-
-            <DialogContent sx={{ textAlign: 'center', padding: '20px 0' }}>
-                {loading ? (
-                    <Loader />
-                ) : (word && (
-                    <Card
-                        key={word.id}
-                        id={word.id}
-                        english={word.english}
-                        transcription={word.transcription}
-                        russian={word.russian}
-                        show={false}
-                    />
-                ))}
-            </DialogContent>
-
-            <DialogActions sx={{ justifyContent: 'center', paddingBottom: '20px' }}>
+        <ModalWrapper ref={ref}>
+            <ModalHeader>
+                <ModalButton onClick={onClose}><ModalButtonIcon icon={faXmark} /></ModalButton>
+            </ModalHeader>
+            {loading ? (
+                <Loader />
+            ) : (word && (
+                <Card
+                    key={word.id}
+                    id={word.id}
+                    english={word.english}
+                    transcription={word.transcription}
+                    russian={word.russian}
+                    show={false}
+                />
+            ))
+            }
+            <ModalFooter>
                 <Checkbox
                     label={`Don't show this again today`}
                     show={true}
@@ -97,7 +62,7 @@ export const Modal = observer(forwardRef(function Modal({ onClose }, ref) {
                     id={checkboxId}
                     checked={!showModalToday}
                 />
-            </DialogActions>
-        </Dialog>
+            </ModalFooter>
+        </ModalWrapper>
     )
 }))
